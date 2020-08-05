da51400d9d7c0dd0858a7cddbea4788627b1dea2
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-05 10:35:00 -0600

    creating a main layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
-
+import Layout from '../components/layout';
 
 const IndexPage = () => (
-  <h1>Gatsby Hotel</h1>
+  <Layout>
+    <h1>Gatsby Hotel</h1>
+  </Layout>
 )
 
 export default IndexPage
