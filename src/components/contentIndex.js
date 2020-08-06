c87f0c70454b682b30e55f8cb4614ee4a2abd401
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-06 13:12:43 -0600

    adding CSS to the main section - index

--- src/components/contentIndex.js
+++ src/components/contentIndex.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+
+const IndexText = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    
+    @media (min-width: 768px) {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      column-gap: 2rem; 
+    }
+
+    p {
+      text-align: justify;
+      line-height: 2;
+    }
+`;
 
 const ContentIndex = () => {
 
@@ -26,12 +47,18 @@ const ContentIndex = () => {
 
     return ( 
         <>
-            <h2>{title}</h2>
+            <h2
+              css={css`
+                text-align: center;
+                font-size: 4rem;
+                margin-top: 4rem;
+              `}
+            >{title}</h2>
 
-            <div>
+            <IndexText>
                 <p>{content}</p>
                 <Image fluid={image.fluid} />
-            </div>
+            </IndexText>
         </>
      );
 }
