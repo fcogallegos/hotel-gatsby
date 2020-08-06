f5501e4cd71e0e8abb34ef237ca989e97865c37f
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-05 18:00:45 -0600

    CSS to the index

--- src/components/imageHotel.js
+++ src/components/imageHotel.js
@@ -8,6 +8,33 @@ const ImageBackground = styled(BackgroundImage)`
     height: 700px;
 `;
 
+const TextImage = styled.div`
+    background-image: linear-gradient( to top, rgba(34,49,63,.8), rgba(34,49,63,.8) );
+    color: #FFF;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        font-size: 4rem;
+        margin: 0%;
+
+        @media (min-width: 992px) {
+            font-size: 5.8rem;
+        }
+    }
+    p {
+        font-size: 2rem;
+
+        @media (min-width: 992px) {
+            font-size: 2.6rem;
+        }
+    }
+`;
+
 const ImageHotel = () => {
 
     const { image } = useStaticQuery(graphql`
@@ -30,7 +57,10 @@ const ImageHotel = () => {
             fluid={image.sharp.fluid}
             fadeIn="soft"
         >
-
+            <TextImage>
+                <h1>Welcome to Hotel Gatsby</h1>
+                <p>The best hotel for your vacations</p>
+            </TextImage>
         </ImageBackground>
      );
 }
