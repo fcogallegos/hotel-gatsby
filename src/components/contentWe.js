70b66a0b28f5d5cf3847f178438660c46a581b1d
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-06 13:49:27 -0600

    adding CSS to We Page

--- src/components/contentWe.js
+++ src/components/contentWe.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+
+const Content = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      column-gap: 2rem; 
+    }
+
+    p {
+      text-align: justify;
+      line-height: 2;
+    }
+`;
 
 
 const ContentWe = () => {
@@ -27,14 +48,20 @@ const ContentWe = () => {
 
     return ( 
         <>
-            <h2>{title}</h2>
-            <div>
+            <h2
+                css={css`
+                    margin-top: 4rem;
+                    text-align: center;
+                    font-size: 4rem;
+                `}
+            >{title}</h2>
+            <Content>
                 <p>{content}</p>
                 <Image 
                     fluid={image.fluid}
-                    
+
                 />
-            </div>
+            </Content>
         </>
      );
 }
