088c0f68eccda13dd7d73f69295e6e7124c1942d
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-07 10:51:37 -0600

    creating the dynamic pages with programation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({actions, graphql, reporter}) => {
+    const result = await graphql(`
+        query {
+            allDatoCmsRoom {
+              nodes {
+               slug
+              }
+            }
+          }
+    `);
+    
+    //console.log(result.data.allDatoCmsRoom.nodes);
+    
+    if(result.errors) {
+        reporter.panic('There are not results ', result.errors);
+    }
 
-// You can delete this file if you're not using it
+    //there are results, create the files
+    const rooms = result.data.allDatoCmsRoom.nodes;
+
+    rooms.forEach(room => {
+        actions.createPage({
+            path: room.slug,
+            component: require.resolve('./src/components/rooms.js'),
+            context: {
+                slug: room.slug
+            }
+        })
+    });
+}
---
+++ src/components/rooms.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RoomTemplate = () => {
+    return ( <h1>Room.js</h1> );
+}
+ 
+export default RoomTemplate;
